1bd7a833cc97b08f0df2d7d2f4e5d80f822032e0
hoang.dinh21@gmail.com <hoang.dinh21@gmail.com@57953c3b-dae6-92d9-b358-e15acec26f1b>
2011-06-04 16:24:13 +0000

    sua JavaHeart -> 4 nguoi choi - khong co computer

---
+++ JavaHeart/src/javaheart/GameDef.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaheart;
+
+/**
+ *
+ * @author kydrenw
+ */
+public class GameDef {
+    public static int GAME_EXCHANGE = 1;
+    public static int GAME_PLAY = 2;
+    public static int GAME_END = 3;
+}
