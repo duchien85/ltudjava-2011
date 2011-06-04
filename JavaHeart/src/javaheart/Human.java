bd3447e27d98c2ccc342db373be58690ab7d6963
hoang.dinh21@gmail.com <hoang.dinh21@gmail.com@57953c3b-dae6-92d9-b358-e15acec26f1b>
2011-06-04 16:55:57 +0000

    Them client va server

--- JavaHeart/src/javaheart/Human.java
+++ JavaHeart/src/javaheart/Human.java
@@ -2,29 +2,30 @@ package javaheart;
 
 import java.util.ArrayList;
 
+public class Human extends Player {
 
-public class Human extends Player{
+    // khoi tao nguoi choi voi ten
+    public Human(String name) {
+        this.name = name;
+        score = 0;
+        handcard = new card[14];
+        playedcard = new Boolean[14];
+        scorecard = new ArrayList<card>();
+        threeCard = new ArrayList<Integer>();
+        ;
+    }
 
-     public Human(String name){
-		this.name= name;
-		score=0;
-		handcard=new card[14];
-		playedcard=new Boolean[14];
-		scorecard=new ArrayList<card>();
-                threeCard = new ArrayList<Integer>();;
-	}
+    public card playACard(int index) {
+        playedcard[index] = true;
+        return handcard[index];
+    }
 
-	public card playACard(int index){
-		playedcard[index]=true;
-		return handcard[index];
-	}
-	
-	public boolean checkAvableRank(card c){
-		for (int i=1;i<=13;i++){			
-			if ((playedcard[i]==false)&&(c.checkSameRank(handcard[i])))
-				return true;			
-		}
-		return false;
-	}
-	
+    public boolean checkAvableRank(card c) {
+        for (int i = 1; i <= 13; i++) {
+            if ((playedcard[i] == false) && (c.checkSameRank(handcard[i]))) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- JavaHeart/src/javaheart/myMouseListener.java
+++ JavaHeart/src/javaheart/myMouseListener.java
@@ -3,17 +3,15 @@ package javaheart;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+public class myMouseListener extends MouseAdapter {
 
+    private int[] sinal;
 
+    public myMouseListener(int[] signal) {
+        this.sinal = signal;
+    }
 
-public class myMouseListener extends MouseAdapter{
-	private int[] sinal;
-	public myMouseListener(int[] signal){
-		this.sinal=signal;
-	}
-	
-	public void mousePressed(MouseEvent e) {				
-		sinal[0]=Integer.parseInt(e.getComponent().getName());
-	}
-	
+    public void mousePressed(MouseEvent e) {
+        sinal[0] = Integer.parseInt(e.getComponent().getName());
+    }
 }
